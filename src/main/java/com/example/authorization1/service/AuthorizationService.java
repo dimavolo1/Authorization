3e646c5e41b88043beef73530157d6f45458bc2a
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-12-05 19:25:45 +0500

    commit

---
+++ src/main/java/com/example/authorization1/service/AuthorizationService.java
@@ -0,0 +1,38 @@
+package com.example.authorization1.service;
+
+import com.example.authorization1.exception.InvalidCredentials;
+import com.example.authorization1.exception.UnauthorizedUser;
+import com.example.authorization1.model.Authorities;
+import com.example.authorization1.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+public class AuthorizationService {
+    UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<Authorities> getAuthorities(String user, String password) {
+        if (isEmpty(user) || isEmpty(password)) {
+            throw new InvalidCredentials("User name or password is empty");
+        }
+        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
+        if (isEmpty(userAuthorities)) {
+            throw new UnauthorizedUser("Unknown user " + user);
+        }
+        return userAuthorities;
+    }
+
+    private boolean isEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private boolean isEmpty(List<?> str) {
+        return str == null || str.isEmpty();
+    }
+}
